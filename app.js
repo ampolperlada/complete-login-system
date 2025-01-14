f53e0e93a32d33bf04116414dd986b7cc58d1a88
Christian Perlada <74958847+ampolperlada@users.noreply.github.com>
2025-01-14 22:26:32 +0800

    note-3

--- app.js
+++ app.js
@@ -16,3 +16,8 @@ testConnection(); // Run the test
 
 //note for this: In summary, the code snippet is a Node.js script for testing a connection to a PostgreSQL database. It can be part of a larger application, including an Express server, 
 // but it is not inherently tied to Express itself.
+
+
+
+
+//NOTE: PACKAGE In summary, the package-lock.json file is a crucial part of a Node.js project that helps manage dependencies, ensuring consistent installations across different environments while providing metadata about the project and its dependencies.
